b5e2baf8bf47b6f7af08f0fd670793228de10cba
dupadhyaya <dup1966@gmail.com>
2018-01-25 07:57:28 +0530

    a

--- ml/dctree1.py
+++ ml/dctree1.py
@@ -47,10 +47,10 @@ graph = graphviz.Source(dot_data)
 graph.render("iris") 
 
 dot_data = tree.export_graphviz(clf, out_file=None, 
-                         feature_names=iris.feature_names,  
-                         class_names=iris.target_names,  
-                         filled=True, rounded=True,  
-                         special_characters=True)  
+    feature_names=iris.feature_names,  
+    class_names=iris.target_names,  
+    filled=True, rounded=True,  
+    special_characters=True)  
 graph = graphviz.Source(dot_data)  
 graph 
 clf.predict(iris.data[:1, :])
