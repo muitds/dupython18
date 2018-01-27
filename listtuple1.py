9343e96662017e8d636d44a1f029c6fc76ca2632
Dhiraj Upadhyaya <dup1966@gmail.com>
2018-01-27 09:44:24 +0530

    a

---
+++ listtuple1.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 26 12:30:08 2018 by Dhiraj Upadhyaya for DS 
+Tuples
+"""
+L1 = [1,2,'DS','Dhiraj', True]
+L2=[i for i in range(9)]
+L2
+sum(L2[:])
+L1[3].upper()
+L1.count()
+sum(L1[0:2])
+
+if L1[4]:
+    print('hello')
+else:
+    print('No Hello')       
+
+if not L1[4]:
+    print('Not True')
+else:
+    print('Hellow')
+
+
+M = ('dept',(23,'MG road'),[50,34,'India'])
+
